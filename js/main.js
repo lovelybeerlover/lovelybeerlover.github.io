11e94b5c68d32a63571c5e450f34b8b67fdf0538
Min Kim <minkim0110@gmail.com>
2015-08-24 11:03:45 -0400

    new website

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-function detailModal() {
-    $('.modal-window').css('display','block');
+function aboutmeModal() {
+	console.log('open');
+    $('#aboutme').css('display','block');
 }
-
-// function close() {
-//     $('.modal-window').toggle();
-// }
-
    $('.modal-window').click(function(){
         $('.modal-window').css('display','none')
     });
+
+function contactModal() {
+	console.log('open2');
+    $('#contact').css('display','block');
+}
+   $('.modal-window').click(function(){
+        $('.modal-window').css('display','none')
+    });
+
+
+jQuery(function( $ ){
+
+	console.log('hello');
+
+	if( $( document ).scrollTop() > 0 ){
+		$( '.nav-menu' ).addClass( 'opac1' );	
+		console.log('hello2');		
+	}
+
+	// Add opacity class to site header
+	$( document ).on('scroll', function(){
+
+		if ( $( document ).scrollTop() > 0 ){
+			$( '.nav-menu' ).addClass( 'opac1' );
+			console.log('hello3');	
+
+		} else {
+			$( '.nav-menu' ).removeClass( 'opac1' );
+			console.log('hello4');		
+		}
+
+	});
+});
